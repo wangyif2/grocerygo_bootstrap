e151687d871f0fa6ef53462d5a5adacf098d82de
robert <robert.yifan.wang@gmail.com>
2013-07-04 12:43:02 -0600

    init bootstrap backbone for grocerygo landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrocerygoBootstrap::Application.config.secret_key_base = '5654b5d413dcee061943c9e3da8c63d7ad471c0672df19cb42ffa62e7d440fe85592a375bac04b6a34cf5530d324216e3aef00eb74c4e7badf7e3f6a772c85f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrocerygoBootstrap::Application.config.session_store :cookie_store, key: '_grocerygo_bootstrap_session'
